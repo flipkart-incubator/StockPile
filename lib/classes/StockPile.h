d73c88e51ede0da2db1eda79d6831e87fe3b4e53
prabodhprakash <prabodh.prakash@flipkart.com>
2015-09-12 11:20:18 +0530

    some indentation changes in documentation.

--- lib/classes/StockPile.h
+++ lib/classes/StockPile.h
@@ -10,9 +10,9 @@
 #import "CacheBuilders.h"
 
 /*!
- @brief This class is used to get specific implementations of caching algorithms.
+    @brief This class is used to get specific implementations of caching algorithms.
  
- @discussion It has all class level methods that are used to return specific implementations of caching algorithms. It supports in memory based cache, in memory + disk based cache, im memory + db based cache.
+    @discussion It has all class level methods that are used to return specific implementations of caching algorithms. It supports in memory based cache, in memory + disk based cache, im memory + db based cache.
  */
 @interface StockPile : NSObject
 
@@ -51,20 +51,20 @@
 + (id<CacheProtocol>) getInMemoryDiskCopyCacheUsingData:(id<DiskCacheDataSource>) dataSource;
 
 /*!
- @brief It returns new instance of a class implemeting CacheProtocol.
+    @brief It returns new instance of a class implemeting CacheProtocol.
  
- @discussion This method is used to return instance of class with <b>in
- memory and disk based caching</b>. This means that the elements that are being cached will also be
- saved in disk. This method ensures that the cached are safe even is case of crash etc. For better performance
- of retrival and saving elements, use database.
+    @discussion This method is used to return instance of class with <b>in
+    memory and disk based caching</b>. This means that the elements that are being cached will also be
+    saved in disk. This method ensures that the cached are safe even is case of crash etc. For better performance
+    of retrival and saving elements, use database.
  
- @code
- [StockPile getInMemoryCacheUsingData:id<DBCacheDataSource> dataSource];
- @endcode
+    @code
+    [StockPile getInMemoryCacheUsingData:id<DBCacheDataSource> dataSource];
+    @endcode
  
- @param dataSource Instance of a class implementing DBCacheDataSource
+    @param dataSource Instance of a class implementing DBCacheDataSource
  
- @return new instance of class implementing CacheProtocol
+    @return new instance of class implementing CacheProtocol
  */
 + (id<CacheProtocol>) getInMemoryDBCopyCacheUsingData:(id<DBCacheDataSource>) dataSource;
 
