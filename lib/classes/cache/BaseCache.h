c7c24047d8031cb62aab06530783e01086a98c50
prabodhprakash <prabodh.prakash@flipkart.com>
2015-09-14 01:48:48 +0530

    Documentation in BaseCache.

--- lib/classes/cache/BaseCache.h
+++ lib/classes/cache/BaseCache.h
@@ -14,23 +14,28 @@
 
 @protocol CacheProtocol <NSObject>
 
-/**
+/*!
+ * This function is used to cache value against a uniquely identifying key.
+ *
  * @param value Value that needs to be cached
+ *
  * @param key Key that needs to be used for retrieval
+ *
+ * @return returns a YES, if the data was properly cached else NO.
  */
 - (BOOL) cacheValue:(Value*) value forKey:(NSString*)key;
 
-/**
+/*!
  * This method will retrieve the cached value against key. If the value is not present in memory,
  * it will look out in the mirrored and then the fallback cache.
  *
- * @param key id<NSCopying, NSMutableCopying, NSSecureCoding> type for which the cached value needs to be retrieved
+ * @param key NSString type for which the cached value needs to be retrieved
  *
  * @return Retrieved cached value as Node. It returns nil if no cached value is found.
  */
 - (Value*) getValueForKey:(NSString*) key;
 
-/**
+/*!
  * This method will clear all values from this cache and the mirrored and fallback cache in a cascading manner. This would be a blocking call
  */
 - (void) clearCacheAndCascade;
@@ -41,16 +46,14 @@
 
 @required
 
-//@property (nonatomic, strong, getter=path) NSString* path;
-
-/**
+/*!
  * @param node Node type that needs to be cached
  */
 - (BOOL) cacheNode: (Node*) node;
 
-/**
+/*!
  * This method will retrieve the cached value against key. If the value is not present in memory,
- * it will look out in database for corresponding value.
+ * it will look out in mirrored or fallback cache in cascading manner.
  *
  * @param key String value for which the cached value needs to be retrieved
  *
@@ -58,7 +61,7 @@
  */
 - (Node*) getNodeForKey:(NSString*) key;
 
-/**
+/*!
  * This method will clear all values from cache
  */
 - (void) clearCache;
@@ -69,7 +72,14 @@
 
 @interface BaseCache : NSObject <CacheProtocol, CacheAlgorithmProtocol>
 
+/*!
+ Holds the fallback cache
+ */
 @property (nonatomic, strong) BaseCache* fallBackCache;
+
+/*!
+ Holds the mirrored cache
+ */
 @property (nonatomic, strong) BaseCache* mirroredCache;
 
 @end
