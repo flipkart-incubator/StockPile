5313c9f860647d5fe31d13ad5729943934105612
prabodhprakash <prabodh.prakash@flipkart.com>
2015-09-14 01:19:23 +0530

    1. Removed un-necessary code. 2. Documentation.

--- lib/classes/cache/inMemory/LRUCache.h
+++ lib/classes/cache/inMemory/LRUCache.h
@@ -12,10 +12,19 @@
 #import "CachingDiskHandler.h"
 #import "BaseCache.h"
 
-
+/*!
+ This class provides standard implementation of LRU Cache.
+ */
 @interface LRUCache : BaseCache
 
+/*!
+The maximum number of elements that can be stored in memory.
+ */
 @property (nonatomic, assign) NSInteger maxElementsInMemory;
+
+/*!
+The maximum memory that can be used to store elements
+ */
 @property (nonatomic, assign) NSInteger maxMemoryAllocated;
 
 @end
--- lib/classes/cache/inMemory/LRUCache.m
+++ lib/classes/cache/inMemory/LRUCache.m
@@ -33,73 +33,6 @@
     return self;
 }
 
-- (instancetype) initWithCapacity: (int) capacity andMemoryLimit:(int) memoryLimit
-{
-    if (capacity <= 0)
-    {
-        @throw [[CachingException alloc] initWithReason:@"Capacity must be greater than 0"];
-    }
-    
-    self = [self init];
-    return self;
-}
-
-/**
- * If initialized using this method, the default memory limit is 100 MB. If memory limit is not
- * according to the requirements, use initWithMemoryLimitInMB: method to initialize the LRUCache
- *
- * @param capacity integer value which tells the quantity of items in cache
- *
- * @return The newly-initialized LRUCache
- */
-- (instancetype) initWithCapacity: (int) capacity
-{
-    if (capacity <= 0)
-    {
-        @throw [[CachingException alloc] initWithReason:@"Capacity must be greater than 0"];
-    }
-    
-    self = [self init];
-    if (self)
-    {
-        _maxElementsInMemory = capacity;
-        _maxMemoryAllocated = 100;
-        _linkedHashMap = [[LinkedHashMap alloc] init];
-    }
-    
-    return self;
-}
-
-/**
- * If initialized using this method, there will be no cap on the quantity of items that can be kept in
- * LRUCache. It is set to INT_MAX
- *
- * @param reason integer value that specifies the cap on memory limit in MB's
- *
- * @return The newly-initialized LRUCache
- */
-- (instancetype) initWithMemoryLimitInMB:(int) memoryLimit
-{
-    if (memoryLimit <= 0)
-    {
-        @throw [[CachingException alloc] initWithReason:@"Memory limit must be greater than 0"];
-    }
-    
-    self = [super init];
-    
-    if (self)
-    {
-        _maxElementsInMemory = INT_MAX;
-        _maxMemoryAllocated = memoryLimit;
-    }
-    
-    return self;
-}
-
-
-/**
- * @param node Node type that needs to be cached
- */
 - (BOOL) cacheNode: (Node*) node
 {
     if (_currentSize == _maxElementsInMemory)
@@ -123,14 +56,6 @@
     return YES;
 }
 
-/**
- * This method will retrieve the cached value against key. If the value is not present in memory,
- * it will look out in database for corresponding value.
- *
- * @param key String value for which the cached value needs to be retrieved
- *
- * @return Retrieved cached value as Node. It returns nil if no cached value is found.
- */
 - (Node*) getNodeForKey:(NSString*) key
 {
     Node* node = [[self linkedHashMap]  getObject:key];
@@ -149,9 +74,6 @@
     return node;
 }
 
-/**
- * This method will clear all values from cache
- */
 - (void) clearCache
 {
     _currentSize = 0;
