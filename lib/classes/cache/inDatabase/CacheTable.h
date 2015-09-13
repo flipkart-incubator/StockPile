2268d1d0ab2fb04a91ad2f3da179dff5665b14bc
prabodhprakash <prabodh.prakash@flipkart.com>
2015-09-14 01:29:33 +0530

    Documentation.

--- lib/classes/cache/inDatabase/CacheTable.h
+++ lib/classes/cache/inDatabase/CacheTable.h
@@ -8,14 +8,29 @@
 
 #import <CoreData/CoreData.h>
 
+/*!
+ This class is a subclass of NSManagedObject and maps to the CacheTable
+ */
 @interface CacheTable : NSManagedObject
 
+/*!
+ The actual data that needs to be stored. This is mandatory.
+ */
 @property (nonatomic, strong) NSData* value;
 
+/*!
+ A key that can uniquely identify the data. This is mandatory
+ */
 @property (nonatomic, strong) NSString* key;
 
+/*!
+ Instance of NSDate post which the data must be invalidated. This is optional.
+ */
 @property (nonatomic, strong) NSDate* ttlDate;
 
+/*!
+ This property holds the time interval - time calculated since saving of data, post which data should be invalidated.
+ */
 @property (nonatomic) NSTimeInterval ttlInterval;
 
 @end
--- lib/classes/cache/inDatabase/CachingDatabaseHandler.h
+++ lib/classes/cache/inDatabase/CachingDatabaseHandler.h
@@ -10,8 +10,14 @@
 #import "Node.h"
 #import "BaseCache.h"
 
+/*!
+ It extends BaseCache and is used to save data to database.
+ */
 @interface CachingDatabaseHandler : BaseCache
 
+/*!
+ Name of database.
+ */
 @property (nonatomic, copy) NSString* dbName;
 
 @end
