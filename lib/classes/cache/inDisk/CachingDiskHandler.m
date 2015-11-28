8f3d2222c388e12c2699e864062f1b819c592564
prabodhprakash <prabodh.prakash@flipkart.com>
2015-11-28 14:13:09 +0530

    Added NSFileManager for handling disk based caching.

--- lib/classes/cache/inDisk/CachingDiskHandler.m
+++ lib/classes/cache/inDisk/CachingDiskHandler.m
@@ -7,6 +7,8 @@
 //
 
 #import "CachingDiskHandler.h"
+#import "CachingException.h"
+#import "NSFileManager+AttributesExtension.h"
 
 @implementation CachingDiskHandler
 
@@ -14,24 +16,67 @@
 
 - (BOOL) cacheNode: (Node*) node
 {
+    BOOL isCached = YES;
+    
     NSString *filePath = [self.filePath stringByAppendingPathComponent:node.key];
     
-    BOOL success = [NSKeyedArchiver archiveRootObject:node.data toFile:filePath];
+    NSData* fileData = [NSKeyedArchiver archivedDataWithRootObject:node.data];
+
+    NSFileManager * fileManager = [NSFileManager defaultManager];
     
-    return success;
+    if (![fileManager fileExistsAtPath:filePath])
+    {
+        [fileManager createFileAtPath:filePath contents:fileData attributes:nil];
+        
+        NSError* error;
+        NSData* ttlDate = [NSKeyedArchiver archivedDataWithRootObject:node.data.ttlDate];
+        
+        [fileManager setData:ttlDate forExtendedAttribute:@"ttlDate" ofItemAtPath:filePath error:&error];
+    }
+    else
+    {
+        NSError* error = nil;
+        
+        [fileManager removeItemAtPath:filePath error:&error];
+        
+        if (!error)
+        {
+            [fileManager createFileAtPath:filePath contents:fileData attributes:nil];
+        }
+        else
+        {
+            isCached = NO;
+        }
+    }
+    
+    return isCached;
 }
 
 - (Node*) getNodeForKey:(NSString*) key
 {
     NSString *filePath = [self.filePath stringByAppendingPathComponent:key];
-    Value* value = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+
+    NSFileManager * fileManager = [NSFileManager defaultManager];
+    
+    NSError* error;
+    NSData* ttlDateArchivedData = [fileManager dataForExtendedAttribute:@"ttlDate" ofItemAtPath:filePath error:&error];
+    
+    NSDate* ttlDate = (NSDate*)[NSKeyedUnarchiver unarchiveObjectWithData:ttlDateArchivedData];
+    
+    Value* value = nil;
+    if ([ttlDate compare:[NSDate date]] != NSOrderedDescending)
+    {
+        NSData* fileData = [fileManager contentsAtPath:filePath];
+        value = (Value*)[NSKeyedUnarchiver unarchiveObjectWithData:fileData];
+    }
+    
     Node* node = [[Node alloc] initWithKey:key value:value];
     return node;
 }
 
 - (void) clearCache
 {
-#warning complete the code
+    #warning complete the code
 }
 
 @end
